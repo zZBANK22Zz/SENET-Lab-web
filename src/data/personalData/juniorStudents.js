cc44859bc578141af1f1e3e63af8ca956e4c3197
zZBANK22Zz <ananda2546@gmail.com>
2025-09-01 13:50:36 +0700

    add info

--- src/data/personalData/juniorStudents.js
+++ src/data/personalData/juniorStudents.js
@@ -11,18 +11,15 @@ const juniorStudents = [
       email: "ananthichai.s@gmail.com",
       profileImage: "/images/team/Ananthichai.JPG",
       bio: "Undergraduate student passionate about web development and cloud technologies. Assisting in research projects while completing bachelor's degree.",
-      researchInterests: [
-        "Software Engineering",
-        "Software Testing"
-      ],
+      researchInterests: ["Software Engineering", "Software Testing"],
       supervisor: "Asst.Prof.Dr. Adisak Intana",
       yearStarted: "2024",
-      yearLevel: "4rd Year"
+      yearLevel: "4rd Year",
     },
     currentWork: {
       title: "Enhanced Test Case Generation Using Fault Tree Analysis and CCTM",
-      description: "Enhanced Falt Tree Analysis into CCTM method"
-    }
+      description: "Enhanced Falt Tree Analysis into CCTM method",
+    },
   },
   {
     personalInfo: {
@@ -36,17 +33,17 @@ const juniorStudents = [
       email: "s6530613008@phuket.psu.ac.th",
       profileImage: "/images/team/tanthai.png",
       bio: "Undergraduate student passionate about web development and cloud technologies. Assisting in research projects while completing bachelor's degree.",
-      researchInterests: [
-        "Software Engineering"
-      ],
+      researchInterests: ["Software Engineering"],
       supervisor: "Asst.Prof.Dr. Adisak Intana",
       yearStarted: "2024",
-      yearLevel: "4rd Year"
+      yearLevel: "4rd Year",
     },
     currentWork: {
-      title: "Formal model generation from pattern based wireless sensor networks",
-      description: "Developing a framework to generate Event-B formal models from reusable pattern-based wireless sensor network designs."
-    }
+      title:
+        "Formal model generation from pattern based wireless sensor networks",
+      description:
+        "Developing a framework to generate Event-B formal models from reusable pattern-based wireless sensor network designs.",
+    },
   },
   {
     personalInfo: {
@@ -60,17 +57,17 @@ const juniorStudents = [
       email: "Pongsakorn10399@gmail.com",
       profileImage: "/images/team/pongsakorn.png",
       bio: "Undergraduate student passionate about web development and cloud technologies. Assisting in research projects while completing bachelor's degree.",
-      researchInterests: [
-        "Software Engineering"
-      ],
+      researchInterests: ["Software Engineering"],
       supervisor: "Asst.Prof.Dr. Adisak Intana",
       yearStarted: "2024",
-      yearLevel: "4rd Year"
+      yearLevel: "4rd Year",
     },
     currentWork: {
-      title: "The development of combinations tools for black block testing techniques",
-      description: "This project presents BlackBoxTestGen, a web-based prototype that integrates three black-box testing techniques Equivalence Class Partitioning, Syntax Testing, and State Transition Testing into a unified platform for automated test case generation."
-    }
+      title:
+        "The development of combinations tools for black block testing techniques",
+      description:
+        "This project presents BlackBoxTestGen, a web-based prototype that integrates three black-box testing techniques Equivalence Class Partitioning, Syntax Testing, and State Transition Testing into a unified platform for automated test case generation.",
+    },
   },
   {
     personalInfo: {
@@ -84,17 +81,17 @@ const juniorStudents = [
       email: "s6630613003@phuket.psu.ac.th",
       profileImage: "/images/team/wittawin.png",
       bio: "Undergraduate student passionate about web development and cloud technologies. Assisting in research projects while completing bachelor's degree.",
-      researchInterests: [
-        "Software Engineering"
-      ],
+      researchInterests: ["Software Engineering"],
       supervisor: "Asst.Prof.Dr. Adisak Intana",
       yearStarted: "2025",
-      yearLevel: "3rd Year"
+      yearLevel: "3rd Year",
     },
     currentWork: {
-      title: "Developing an ontology for guiding quality assurance according to ISO/IEC 29110",
-      description: "ISO/IEC 29110 is an international standard for software quality management and development in very small entities (VSEs). An ontology-based knowledge map helps organize its complex requirements and supports intelligent systems for quality assurance."
-    }
+      title:
+        "Developing an ontology for guiding quality assurance according to ISO/IEC 29110",
+      description:
+        "ISO/IEC 29110 is an international standard for software quality management and development in very small entities (VSEs). An ontology-based knowledge map helps organize its complex requirements and supports intelligent systems for quality assurance.",
+    },
   },
   {
     personalInfo: {
@@ -108,18 +105,40 @@ const juniorStudents = [
       email: "s6530613016@phuket.psu.ac.th",
       profileImage: "/images/team/chanakarn.png",
       bio: "Undergraduate student passionate about web development and cloud technologies. Assisting in research projects while completing bachelor's degree.",
-      researchInterests: [
-        "Web Development",
-      ],
+      researchInterests: ["Web Development"],
       supervisor: "Kuljaree Tantayakul",
       yearStarted: "2024",
-      yearLevel: "4rd Year"
+      yearLevel: "4rd Year",
     },
     currentWork: {
       title: "Stock Web Application",
-      description: "To create web application for stock management in Price of Songkhla University"
-    }
-  }
+      description:
+        "To create web application for stock management in Price of Songkhla University",
+    },
+  },
+  {
+    personalInfo: {
+      id: "chanakarn-khunkrai",
+      firstName: "Chanakarn",
+      lastName: "Khunkrai",
+      fullName: "Chanakarn Khunkrai",
+      title: "Ms.",
+      position: "Undergraduate Research Assistant",
+      department: "Digital Engineering",
+      email: "s6530613016@phuket.psu.ac.th",
+      profileImage: "/images/team/chanakarn.png",
+      bio: "Undergraduate student passionate about web development and cloud technologies. Assisting in research projects while completing bachelor's degree.",
+      researchInterests: ["Web Development"],
+      supervisor: "Kuljaree Tantayakul",
+      yearStarted: "2024",
+      yearLevel: "4rd Year",
+    },
+    currentWork: {
+      title: "Stock Web Application",
+      description:
+        "To create web application for stock management in Price of Songkhla University",
+    },
+  },
 ];
 
 export default juniorStudents;
